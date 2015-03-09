29177dafb397b27ab4d8d32e42070b6fc02d8b9e
Seonaid <seonaidl@gmail.com>
2015-03-09 20:11:31 -0300

    using passport to register new user. does not handle errors

--- config/passport.js
+++ config/passport.js
@@ -5,8 +5,20 @@
 
 var LocalStrategy = require('passport-local').Strategy;
 var TwitterStrategy = require('passport-twitter').Strategy;
+var models = require('../models');
+var bcrypt = require('bcrypt-nodejs');
 
 module.exports = function(passport){
+    passport.serializeUser(function(user, done){
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done){
+        User.findById(id, function(err, user){
+            done(err, user);
+        });
+    });
+
     passport.use('register', new LocalStrategy({
         usernameField : 'email', // this is an override of the default, which is username
         passwordField : 'password',
@@ -14,8 +26,40 @@ module.exports = function(passport){
     },
     function(req, email, password, done) {
         // async User.findOne (this is part of mongoose???) won't fire unless data is sent back
-        console.log('in passport file looking up user by ' + local.email);
+        console.log('in passport file looking up user by ' + email);
+        var hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+        console.log('hash: ' + hashedPassword);
+        models.User.create({
+                email: email,
+                hashedPassword: hashedPassword
+            }).then(
+            function(user){
+                console.log(user.email + ' added');
+                return done(null, user);
+            },
+            function(error){
+                console.log('adding user failed');
+            });
 
+         }));
 
-    }));
+    passport.use('local', new LocalStrategy({
+            usernameField : 'email', // this is an override of the default, which is username
+            passwordField : 'password',
+            passReqToCallback: true
+        },
+        function(req, email, password, done) {
+            models.User.findOne({ email: email }, function (err, user) {
+                if (err) { return done(err); }
+                if (!user) {
+                    return done(null, false, { message: 'Incorrect username.' });
+                }
+                if (!user.validPassword(password)) {
+                    return done(null, false, { message: 'Incorrect password.' });
+                }
+                return done(null, user);
+            });
+        }
+    ));
+
 }
--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,6 @@ var sequelize = require('sequelize');
 var bcrypt = require('bcrypt-nodejs');
 var bodyParser = require('body-parser');
 
-
-// configures passport
-require('../config/passport')(passport);
-
-app.use(passport.initialize());
-
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'UseThis' });
@@ -23,29 +17,21 @@ router.get('/register', function(req, res) {
     res.render('register');
 });
 
-router.post('/register', bodyParser.urlencoded({extended: true}), function(req, res){
-    var newUser = [];
-    newUser.email = req.body.email;
-    newUser.password = req.body.password;
-    newUser.hashedPassword = bcrypt.hashSync(newUser.password, bcrypt.genSaltSync(8), null);
-
-    models.User.create({
-        email: newUser.email,
-        hashedPassword: newUser.hashedPassword
-    }).success(function(user){
-        res.send('added ' + user.email + ' to the database')
-    });
-    //
-    //console.log('adding new user to database: ' + newUser.email);
-    //console.log('saving hashed password: ' + newUser.hashedPassword);
-
-//    res.render('index', {title: 'Success'});
-
-
-});
+router.post('/register',passport.authenticate('register', {
+    successRedirect: '/',
+    failureRedirect: '/register'
+}));
 
 router.get('/login', function(req, res){
     res.render('login');
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res){
+    if(!user){
+        res.render('login');
+    } else {
+        res.render('fridge', {name: user.name});
+    }
 });
 
 module.exports = router;
